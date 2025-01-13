0f8d44da23b33f7622fca4200bfc2ec19135e189
Rafee <rafeehasan88@gmail.com>
2025-01-13 09:56:57 +0600

    simple routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter , RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <div>Hello from react router</div>
+  },
+  {
+    path:'/about',
+    element: <div>I am in the About Page</div>
+  },
+  {
+    path:'contact',
+    element: <div>I am in the Contact Page</div>
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router = {router}></RouterProvider>
   </StrictMode>,
 )
