930de9290865660eaaf5dd2577aa03d32ab2554f
Rafee <rafeehasan88@gmail.com>
2025-01-14 11:09:18 +0600

    Navigate on Click, useNavigate, Not Found and useParams

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Post = ({post}) => {
     const postStyle = {
@@ -7,13 +7,18 @@ const Post = ({post}) => {
         borderRadius: '20px',
         
     };
+    const handleShowDetails = ()=>{
+        navigate(`/post/${id}`); 
+
+    };
     const{id , title} = post;
+    const navigate = useNavigate();
     return (
         <div style={postStyle}>
             <h4>Post of Id: {id}</h4>
             <p>{title}</p>
             <Link to={`/post/${id}`}>Post Details</Link>
-            <Link to={`/post/${id}`}><button>Show Details</button></Link>
+            <button onClick={handleShowDetails}>Click to see details</button>
         </div>
     );
 };
--- src/Components/PostDetails/PostDetails.jsx
+++ src/Components/PostDetails/PostDetails.jsx
@@ -1,14 +1,23 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate, useParams } from "react-router-dom";
 
 const PostDetails = () => {
     const post = useLoaderData();
+    const {postId} = useParams();
+    const navigate= useNavigate();
     const {id , title , body} = post;
+    console.log(postId);
+    const handleGoBack= ()=>{
+
+        navigate(-1);
+
+    };
     return (
         <div>
             <h3>Post Details About: </h3>
             <h3>Post Id: {id}</h3>
             <p>Post Title: {title}</p>
             <p>{body}</p>
+            <button onClick={handleGoBack}>Go Back</button>
 
            
 
